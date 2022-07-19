0d8ad22995dbe1b3718223f44dd0195ff0931036
Gabriel Stamato <stamato.freelance@caminoeducation.com>
2022-07-19 08:55:45 -0300

    feat: create constructor and getters and setters tests

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,27 +1,110 @@
-import { Category, CategoryProps } from './category'
+import { Category, CategoryProps } from "./category";
+import { omit } from "lodash";
 
-describe('Category Unit Tests', () => {
+describe("Category Unit Tests", () => {
   const mockCategory: CategoryProps = {
-    name: 'Movie',
-    description: 'description',
+    name: "Movie",
+    description: null,
     is_active: true,
-    created_at: new Date()
-  }
+  };
 
-  test('category s constructor', () => {
-    const category = new Category(mockCategory)
-    expect(category.props).toStrictEqual(mockCategory)
-  })
-
-  test('category s constructos with only name', () => {
-    const created_at = new Date()
-    const category = new Category({name: 'Movie name', created_at})
-
-    expect(category.props).toStrictEqual({
-      name: 'Movie name',
+  test("category s constructor", () => {
+    let category = new Category({ name: "Movie name" });
+    let props = omit(category.props, "created_at");
+    expect(props).toStrictEqual({
+      name: "Movie name",
       description: null,
       is_active: true,
-      created_at
-    })
+    });
+    expect(category.props.created_at).toBeInstanceOf(Date);
+
+    category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+    });
+    let created_at = new Date();
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: false,
+      created_at,
+    });
+
+    category = new Category({
+      name: "Movie",
+      description: "other description",
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      description: "other description",
+    });
+
+    category = new Category({
+      name: "Movie",
+      is_active: true,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      is_active: true,
+    });
+
+    created_at = new Date();
+    category = new Category({
+      name: "Movie",
+      created_at,
+    });
+    expect(category.props).toMatchObject({
+      name: "Movie",
+      created_at,
+    });
+  });
+
+  test("category s getter name field", () => {
+    const category = new Category({ name: "Movie" });
+    expect(category.name).toBe('Movie')
+  });
+
+  test("category s getter and setter description field", () => {
+    let category = new Category({ name: "Movie", description: 'some description' });
+    expect(category.description).toBe('some description')
+
+    category = new Category({ name: "Movie"});
+    expect(category.description).toBeNull()
+
+    category['description'] = 'other description'
+    expect(category.description).toBe('other description')
+
+    category['description'] = undefined
+    expect(category.description).toBeNull()
+
+    category['description'] = null
+    expect(category.description).toBeNull()
+  });
+
+  test("category s getter and setter is_active field", () => {
+    let category = new Category({ name: "Movie", is_active: false });
+    expect(category.is_active).not.toBeTruthy()
+
+    category = new Category({ name: "Movie" });
+    expect(category.is_active).toBeTruthy()
+
+    category['is_active'] = true
+    expect(category.is_active).toBeTruthy()
+
+    category['is_active'] = undefined
+    expect(category.is_active).toBeTruthy()
+
+    category['is_active'] = null
+    expect(category.is_active).toBeTruthy()
+  });
+
+  test("category s getter created_at field", () => {
+    let category = new Category({ name: "Movie" });
+    expect(category.created_at).toBeInstanceOf(Date)
+
+    const created_at = new Date()
+    category = new Category({ name: "Movie", created_at });
+    expect(category.created_at).toBe(created_at)
   })
-})
+});
